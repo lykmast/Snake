2f71c41ba4d690022d85997a6dcb359816f76717
lykmast <lykmast@gmail.com>
2024-02-14 17:39:06 +0200

    Make grid smaller.

--- src/main/java/com/lykmast/Snake/Main.java
+++ src/main/java/com/lykmast/Snake/Main.java
@@ -2,7 +2,7 @@ package com.lykmast.Snake;
 
 public class Main {
     public static void main(String[] args) {
-      WindowConsole theConsole = new WindowConsole(40, 40);
+      WindowConsole theConsole = new WindowConsole(25, 25);
       theConsole.playOnThread();
     }
 }
