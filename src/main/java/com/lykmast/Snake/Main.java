8980b17b624997f549e595e14e37b2be6758e156
lykmast <lykmast@gmail.com>
2024-02-14 13:39:19 +0200

    Remove stub print.

--- src/main/java/com/lykmast/Snake/Main.java
+++ src/main/java/com/lykmast/Snake/Main.java
@@ -2,7 +2,6 @@ package com.lykmast.Snake;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Stub main.");  
       WindowConsole theConsole = new WindowConsole(40, 40);
       theConsole.play();
     }
